190d2bf8be20a5cfb2ec698ad3f7c2060b34b29e
amrit <rit.regmi@gmail.com>
2021-03-03 21:45:02 +0200

    added crossorigin support

--- backend/src/main/java/com/amrit/backend/Controller/ApiController.java
+++ backend/src/main/java/com/amrit/backend/Controller/ApiController.java
@@ -1,19 +1,23 @@
 package com.amrit.backend.Controller;
+
 import com.amrit.backend.AlphavantageApi;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8080") 
 @RestController
 @RequestMapping(path="/")
 public class ApiController {
   @Autowired
   private AlphavantageApi api;
   
+ 
   @GetMapping(path="/query", produces = "application/json")
   public JsonNode getFinancialData(@RequestParam String function, String symbol, @RequestParam(required = false) String interval, String outputsize) throws Exception {
     JsonNode data = api.fetchFinancialData(function, symbol, interval, outputsize, true);
@@ -25,7 +29,7 @@ public class ApiController {
     JsonNode data = api.fetchQuote(function, symbol,true);
     return data;
   }
-  
+
   @GetMapping(path="/search", produces = "application/json")
   public JsonNode getSearch(@RequestParam String function, @RequestParam String keywords) throws Exception {
     JsonNode data = api.search(function,keywords);
